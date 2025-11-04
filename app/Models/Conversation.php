c9d4b68848b5a68cf909bd5fc59bd9ca24190caf
Zyp655 <leehieu655@gmail.com>
2025-11-04 20:31:41 +0700

    feat: add ChatParticipant model and NewChatMessage event; update Conversation model to use ChatParticipant and add user conversations relationship

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models; 
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,15 +11,16 @@ class Conversation extends Model
 
     protected $fillable = ['channel_name'];
 
+
+    public function participants()
+    {
+        
+        return $this->hasMany(ChatParticipant::class);
+    }
+
     
     public function messages()
     {
         return $this->hasMany(Message::class);
-    }
-
-    
-    public function lastMessage()
-    {
-        return $this->hasOne(Message::class)->latest('created_at');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens; 
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
 class User extends Authenticatable
 {
@@ -61,5 +62,13 @@ class User extends Authenticatable
     {
         $type = $this->role;
         return "{$type}-{$this->id}";
+    }
+    public function conversations(): MorphToMany
+    {
+        return $this->morphToMany(
+            Conversation::class,      
+            'participant',            
+            'chat_participants'        
+        );
     }
 }
